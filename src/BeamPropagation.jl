494fdd945ba1df29db0396a26ebe8c73f6e72051
KoheiToyoda <toyoda@chiba-u.jp>
2020-08-18 13:50:00 +0900

    参考文献追加。 md追記。 関数枠だけ追加

--- src/BeamPropagation.jl
+++ src/BeamPropagation.jl
@@ -1,9 +1,45 @@
+#計算に関係ないパッケージ
+using Pkg 
+
 include("parameter.jl")
+
+#計算に使うパッケージ
+using LinearAlgebra
+using SparseArrays
+using Plots
 
 # 計算条件###################
 #計算レンジ
 param_range = struct_range(xwidth = 100, ywidth = 100, zwidth = 100,trange = 50)
 #計算ステップ
-param_step = struct_step(( xstep = 0.1, ystep = 0.1, zstep = 0.05)
+param_step = struct_step(xstep = 0.1, ystep = 0.1, zstep = 0.05)
 #材料情報
-param_material = struct_materiarl((nb = 1.5, Δn0 = 0.01, τ = 0.1,α = 0)
+param_material = struct_materiarl(nb = 1.5, Δn0 = 0.01, τ = 0.1,α = 0)
+
+println("計算環境")
+versioninfo()
+Pkg.status()
+println("計算条件")
+@show param_range
+@show param_step
+@show param_material
+
+#P
+@time function returnPades(T::Int16,N::Int16)
+end
+
+#ADIのX方向差分
+@time function calcStep1()
+end
+
+#ADIのY方向差分
+@time function calcStep2()
+end
+
+@time function showMode()
+end
+
+@time function retN()
+end
+
+
