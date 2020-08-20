553b1f734a59bd075a3c44afc74b4fec5b16b0d5
Kohei TOYODA <ktoyoda1984@gmail.com>
2020-08-21 08:47:20 +0900

    Main進捗

--- src/BeamPropagation.jl
+++ src/BeamPropagation.jl
@@ -21,7 +21,7 @@ param_N = struct_N(Nx,Ny,Nz,Nt)
 #材料情報
 param_material = struct_materiarl(nb = 1.5, Δn0 = 0.01, τ = 0.1,α = 0)
 param_mode = struct_vortex_mode(l=1, p=1)
-param_beam = struct_beam_param()
+param_beam = struct_beam()
 
 #ビーム情報
 param_beam = struct_beam
@@ -41,11 +41,28 @@ println("計算条件")
 end
 
 #ADIのX方向差分
-@time function calcStep1()
+@time function calcStep1(F_k11, F_kp12)
+
+    for i in range(Nx)
+        for j in range(Ny)
+            
+        end
+    end
+    boundary_set(F_kp12)
+    return F_kp12
 end
 
 #ADIのY方向差分
-@time function calcStep2()
+@time function calcStep2(F_k12, F_kp21)
+    
+    for i in range(Nx)
+        for j in range(Ny)
+            
+        end
+    end
+    boundary_set(F_kp21)
+    return F_kp21
+
 end
 
 @time function retN()
@@ -64,6 +81,22 @@ function initial_set(M::struct_vortex_mode,B::struct_beam,)
 end
 function main()
     #セル個数
-    F0 = zeros(param_N.Nx,param_N.Ny)
-    F0  = initial_set(param_mode,param_beam)
+    F0 = zeros(param_N.Nx, param_N.Ny, param_N.Nz)
+    F0[:,:,0]  = initial_set(param_mode,param_beam)
+    F_result = zeros(param_N.Nx,param_N.Ny,param_N.Nz,param_N.Nt)
+    
+    #F_k_1stは現在のF_k
+    #F_k_2ndは更新されたF_k
+    F_k_1st = zeros(param_N.Nx,param_N.Ny)
+    F_k_2nd = zeros(param_N.Nx,param_N.Ny)
+
+    #左辺は更新されたF_k_1stが入る。
+    #F_k_2ndは関数内部で毎回宣言した方がいいのか、
+    #それとも一度宣言して引数として与えた方がいいのか（いまはこれ
+    #は要検証。
+    F_k_1st = calcStep1(F_k_1st,F_k_2nd)
+    F_k_1st = calcStep1(F_k_1st,F_k_2nd)
+    #push(F_k_1st )
+
+
 end
