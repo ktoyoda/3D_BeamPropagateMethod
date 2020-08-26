7511018a7c51a2eec7ff77b07170e82ccb190796
KoheiToyoda <toyoda@chiba-u.jp>
2020-08-26 11:48:22 +0900

    Aに境界条件適用。Bの方はどうしたらいいんだろう。

--- src/BeamPropagation.jl
+++ src/BeamPropagation.jl
@@ -8,6 +8,7 @@ using LinearAlgebra
 using Plots
 using KTOptical
 using .Param
+using BenchmarkTools
 #using FileIO
 # 計算条件###################
 #計算レンジ 
@@ -45,27 +46,42 @@ function returnPades(T::Integer,N::Integer)
 end
 
 #ビームの集光をテーパー型の屈折率分布として表現する。
-function setinitN(N::Array{ComplexF64,2},NA:,material.n)
-
+function setinitN(N::Array{ComplexF64,2},NA,n)
 
 end
 
 #ADIの未知数Y方向 定数X方向 差分
-@time function calcStep1(F_k11, F_kp12)
+#F_k11 既知のビーム伝搬
+#F_kp12 未知のビーム伝搬の格納先
+function calcStep1(F_k11, F_kp12)
     k0 = 2π / beam.wavelength
     a = -1/(step.y)^2
     b = 1im*4*k0*retN()/step.z + 2/step.y^2 - k0^2(material.n^2-retN()^2)/2
     c = 1/(step.x)^2
     d = 1im*4*k0*retN()/step.z - 2/step.x^2 + k0^2(material.n^2-retN()^2)/2
-    B = zeros(ComplexF64,Nx,1)
+    B = zeros(ComplexF64,N.x,1)
     #透明境界条件を使う場合、2からN-1 でいいのかしら？
-    for j in 2:N.y-1
+    for j in 1:N.y
         #Ax=BのA, z = k+1を作る。
-        A = diagm(N.y,N.y, fill(b, N.y)) + diagm(N.y, N.y, 1 => fill(a,N.y-1)) + 
+        A = diagm(N.y,N.y, fill(b, N.y)) + diagm(N.y, N.y, 1 => fill(a,N.y-1)) +
                 diagm(N.y,N.y, -1 => fill(a,N.y-1))
+        #透明境界条件(TBC) for A#######
+        kxr = -1/(1im * step.y)*log(F_k11[1,j]/F_k11[2,j])
+        kxl = -1/(1im * step.y)*log(F_k11[N.y,j]/F_k11[N.y-1,j])
+        A[1,1] += exp(1im * kxr *(-step.y))
+        A[N.y,N.y] += exp(1im * kxl *(-step.y))
+        #########################
         for i in 2:N.x-1
             B[i] = c*F_k11[N.y * j + i]+d*(F_k11[N.y*j + i-1]+F_k11[N.y*j + i+1])
         end
+
+        #透明境界条件(TBC) for B#######
+        kxr = -1/(1im * step.y)*log(F_k11[1,j]/F_k11[2,j])
+        kxl = -1/(1im * step.y)*log(F_k11[N.y,j]/F_k11[N.y-1,j])
+        A[1,1] += exp(1im * kxr *(-step.y))
+        A[N.y,N.y] += exp(1im * kxl *(-step.y))
+        #########################
+
         F_kp12[:,j] = B\A
     end
     return F_kp12
@@ -128,12 +144,10 @@ function main()
     F_k_1st = zeros(N.x, N.y)
     F_k_2nd = zeros(N.x, N.y)
 #    #左辺は更新されたF_k_1stが入る。
-    #F_k_2ndは関数内部で毎回宣言した方がいいのか、
-    #それとも一度宣言して引数として与えた方がいいのか（いまはこれ
-    #は要検証。
+
     F_k_1st = calcStep1(F_k_1st,F_k_2nd)
     F_k_2nd = calcStep2(F_k_1st,F_k_2nd)
-    #push(F_k_1st )
+    "done"
 end
 
 @time main()
